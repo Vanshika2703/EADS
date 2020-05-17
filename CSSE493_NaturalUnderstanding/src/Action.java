bb96c62ad449fddd89fdc52d31707c0ae39b5fa1
Vanshika2703 <reddyvs@rose-hulman.edu>
2020-05-17 12:29:55 -0400

    commit to check

---
+++ CSSE493_NaturalUnderstanding/src/Action.java
@@ -0,0 +1,41 @@
+
+public class Action {
+	private String action;
+
+	public Action() {
+		action = "";
+	}
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Action other = (Action) obj;
+		if (action == null) {
+			if (other.action != null)
+				return false;
+		} else if (!action.equals(other.action))
+			return false;
+		return true;
+	}
+
+}
---
+++ CSSE493_NaturalUnderstanding/src/EmotionKey.java
@@ -0,0 +1,18 @@
+
+public class EmotionKey {
+	private Emotion emotion;
+	private Action defaultAction;
+	
+	public Emotion getEmotion() {
+		return emotion;
+	}
+	public void setEmotion(Emotion emotion) {
+		this.emotion = emotion;
+	}
+	public Action getDefaultAction() {
+		return defaultAction;
+	}
+	public void setDefaultAction(Action defaultAction) {
+		this.defaultAction = defaultAction;
+	}
+}
